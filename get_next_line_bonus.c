3538d3ffd01243afcc7b5984d50f842c788ffcc6
Christopher Lyon <clyon@c3r8s2.42luxembourg.lu>
2025-08-16 19:31:25 +0200

    committing final back to Alchemist_git

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -110,3 +110,39 @@ static char	*_03_ft_strchr(char *s, int c)
 		return ((char *)&s[i]);
 	return (NULL);
 }
+
+/*#include <stdio.h>
+#include <fcntl.h>
+int	main(void)
+{
+	int		fd1 = open("test1.txt", O_RDONLY);
+	int		fd2 = open("test2.txt", O_RDONLY);
+	char	*line1;
+	char	*line2;
+
+	if (fd1 < 0 || fd2 < 0)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	while (1)
+	{
+		line1 = get_next_line(fd1);
+		line2 = get_next_line(fd2);
+		if (!line1 && !line2)
+			break ;
+		if (line1)
+		{
+			printf("[File1] %s", line1);
+			free(line1);
+		}
+		if (line2)
+		{
+			printf("[File2] %s", line2);
+			free(line2);
+		}
+	}
+	close(fd1);
+	close(fd2);
+	return (0);
+}*/
